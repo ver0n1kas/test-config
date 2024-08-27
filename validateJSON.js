50748c849e3abf17fd1f7c4abd8bd972a49a711e
C9Cli <you@example.com>
2024-08-27 09:42:43 -0600

    testing for errors

--- validateJSON.js
+++ validateJSON.js
@@ -1,29 +1,76 @@
 const Ajv = require("ajv")
 const ajv = new Ajv({allErrors: true})
-const fs = require('node:fs');
+const fs = require('fs');
+const fsp = require('node:fs/promises');
+var path = require('path');
 
-validateAgainstSchema(process.argv[2],process.argv[3])
+const errors = []
 
-const schemaMapping = {"org": "org-schema",
-  "orgstate" : "orgstate-schema"
+const schemaMapping = {"org-schema": ".*\\Org\.json$",
+  "orgstate-schema" : ".*\\Org[0-9]*_OrgState\.json$"
 }
 const schemaPath = "schemas"
-function validateAgainstSchema(schemaFile, jsonFile) {
-  fs.readFile(schemaFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    fs.readFile(jsonFile, 'utf8', (err, jsonToValidate) => {
-      if (err) {
-        console.error(err);
-        return;
+
+async function performValidation() {
+  var files = await fs.readdirSync(schemaPath);
+
+  for (i in files) {
+    var schemaFile = files[i]
+    var pattern = schemaMapping[schemaFile.replace(".json", "")]
+    if (pattern) {
+      var filesToValidate = await walkSync(pattern, ".", []);
+      for (f in filesToValidate) {
+        await validateAgainstSchema(path.join(schemaPath, schemaFile), filesToValidate[f])
       }
-      const schema = JSON.parse(data);
-      const validate = ajv.compile(schema)
-      const valid = validate(JSON.parse(jsonToValidate))
-      if (valid) console.log("Valid!")
-      else console.log("Invalid: " + ajv.errorsText(validate.errors))
-    });
-  });
+    }
+  }
+
+  if (errors.length > 0) {
+    var errorString = "";
+    for (e in errors) {
+      errorString += errors[e]
+      errorString += "\n"
+    }
+    throw new Error(errorString)
+  } 
 }
+async function walkSync(pattern, dir, filelist) {
+  var files = await fs.readdirSync(dir);
+  filelist = filelist || [];
+  for (i in files) {;
+    var file = files[i];
+    if (fs.statSync(path.resolve(dir, file)).isDirectory()) {
+      filelist = await walkSync(pattern, path.join(dir, file), filelist);
+    }
+    else {
+      if (file.match(pattern)) {
+        filelist.push(path.resolve(dir, file));
+      }
+    }
+  }
+  return filelist;
+};
+
+async function validateAgainstSchema(schemaFile, jsonFile) {
+  var schemaContents =  await fsp.readFile(schemaFile,  'utf8');
+  var jsonToValidate = await fsp.readFile(jsonFile, 'utf8')
+  if (schemaContents == null || schemaContents == '') {
+    errors.push("Schema file " + schemaFile + " is empty")
+  }else if (jsonToValidate == null || jsonToValidate == '') {
+    errors.push("File being validated " + jsonToValidate + " is empty")
+  } else {
+    try {
+      var tempJsonSource = schemaFile
+      const schema = JSON.parse(schemaContents);
+      const validate = ajv.compile(schema)
+      tempJsonSource = jsonFile
+      const valid = validate(JSON.parse(jsonToValidate))
+      if (!valid){
+        errors.push("Invalid: " + ajv.errorsText(validate.errors))
+      }
+    } catch (error) {
+      errors.push("Bad json file: " + tempJsonSource  + "\n" + error)
+    }
+  }
+}
+performValidation();
