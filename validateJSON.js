1858d91893e802dbc6ead5260065637062df02e0
C9Cli <you@example.com>
2024-08-27 10:00:32 -0600

    testing for errors one more time

--- validateJSON.js
+++ validateJSON.js
@@ -66,7 +66,7 @@ async function validateAgainstSchema(schemaFile, jsonFile) {
       tempJsonSource = jsonFile
       const valid = validate(JSON.parse(jsonToValidate))
       if (!valid){
-        errors.push("Schema validation faled for " + jsonFile + ". " + ajv.errorsText(validate.errors))
+        errors.push("Schema validation failed for " + jsonFile + ". " + ajv.errorsText(validate.errors))
       }
     } catch (error) {
       errors.push("Bad json file: " + tempJsonSource  + "\n" + error)
